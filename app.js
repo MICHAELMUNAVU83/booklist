adb765bb08f9b2f7425cf1e9d0a372f923881e1e
trance <michaelmunavu83@gmail.com>
2022-05-24 08:49:20 +0300

    Add functionality for delete button

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-const add = document.getElementById('add');
+const add = document.getElementById("add");
 const awesomeBooks = [];
-add.addEventListener('click', () => {
-  const title = document.getElementById('title');
-  const author = document.getElementById('author');
+add.addEventListener("click", () => {
+  const title = document.getElementById("title");
+  const author = document.getElementById("author");
 
   const book = {
     Title: title.value,
@@ -11,14 +11,14 @@ add.addEventListener('click', () => {
 
   awesomeBooks.push(book);
 
-  localStorage.setItem('books', JSON.stringify(awesomeBooks));
+  localStorage.setItem("books", JSON.stringify(awesomeBooks));
 
-  if (title === '' || author === '') {
-    alert('tcuvybu');
+  if (title === "" || author === "") {
+    console.log("cvyb");
   } else {
-    const list = document.getElementById('book-list');
+    const list = document.getElementById("book-list");
 
-    const newDiv = document.createElement('div');
+    const newDiv = document.createElement("div");
 
     newDiv.innerHTML += `
       <p>${book.Title}</p>
@@ -30,7 +30,21 @@ add.addEventListener('click', () => {
 
     list.appendChild(newDiv);
 
-    document.getElementById('title').value = '';
-    document.getElementById('author').value = '';
+    document.getElementById("title").value = "";
+    document.getElementById("author").value = "";
   }
+});
+
+document.getElementById("book-list").addEventListener("click", function (e) {
+  console.log(e.target);
+
+  if (e.target.classList.contains("delete")) {
+    e.target.parentElement.remove();
+  }
+  awesomeBooks.forEach((book, i) => {
+    if (e.target.parentElement.firstElementChild.textContent === book.Title) {
+      awesomeBooks.splice(i, 1);
+    }
+  });
+  localStorage.setItem("books", JSON.stringify(awesomeBooks));
 });
